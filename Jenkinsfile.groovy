24eaa32a290b332ab0aa46c095b9dcc58f798523
Rob <brem_robert@hotmail.com>
2018-05-24 17:50:26 +0200

    [REFAC] try to use sidecar container

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -23,7 +23,7 @@ podTemplate(label: 'mypod', containers: [
 
             def kc = 'kubectl'
             def containerPath = '/nexus-data'
-            def containerName = 'nexus'
+            def containerName = 'clone-repo'
             def podLabel = 'app=nexus'
             def repositoryUrl = 'bitbucket.org/khinkali/nexus_backup'
             container('kubectl') {
