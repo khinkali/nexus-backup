385f152015aac3a5816293ed2b2ecef8fedb0fa5
Rob <brem_robert@hotmail.com>
2018-06-01 13:51:32 +0200

    [MONITOR] send slack message on fail

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -6,31 +6,35 @@ podTemplate(label: 'mypod', containers: [
         volumes: [
                 hostPathVolume(mountPath: '/var/run/docker.sock', hostPath: '/var/run/docker.sock'),
         ]) {
-    node('mypod') {
-        properties([
-                buildDiscarder(
-                        logRotator(artifactDaysToKeepStr: '',
-                                artifactNumToKeepStr: '',
-                                daysToKeepStr: '',
-                                numToKeepStr: '30'
-                        )
-                ),
-                pipelineTriggers([cron('0 2 * * *')])
-        ])
+    try {
+        node('mypod') {
+            properties([
+                    buildDiscarder(
+                            logRotator(artifactDaysToKeepStr: '',
+                                    artifactNumToKeepStr: '',
+                                    daysToKeepStr: '',
+                                    numToKeepStr: '30'
+                            )
+                    ),
+                    pipelineTriggers([cron('0 2 * * *')])
+            ])
 
-        stage('create backup') {
-            currentBuild.displayName = getTimeDateDisplayName()
+            stage('create backup') {
+                currentBuild.displayName = getTimeDateDisplayName()
 
-            def kc = 'kubectl'
-            def containerPath = '/nexus-data'
-            def containerName = 'nexus'
-            def podLabel = 'app=nexus'
-            def repositoryUrl = 'bitbucket.org/khinkali/nexus_backup'
-            container('kubectl') {
-                backup(podLabel, containerName, containerPath, repositoryUrl, kc)
+                def kc = 'kubectl'
+                def containerPath = '/nexus-data'
+                def containerName = 'nexus'
+                def podLabel = 'app=nexus'
+                def repositoryUrl = 'bitbucket.org/khinkali/nexus_backup'
+                container('kubectl') {
+                    backup(podLabel, containerName, containerPath, repositoryUrl, kc)
+                }
             }
-        }
 
+        }
+    } catch (all) {
+        slackSend "Build Failed - ${env.JOB_NAME} ${env.BUILD_NUMBER} (<${env.BUILD_URL}|Open>)"
     }
 }
 
